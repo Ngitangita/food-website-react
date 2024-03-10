7358cb3d15d8b19f232b067ce5b9236d828e6d6c
Ngitangita <hei.elise.3gmail.com>
2024-03-10 16:13:27 +0300

    add active NavLink

--- src/componants/navbar/Navbar.jsx
+++ src/componants/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import style from './navbar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUtensils, faHeart, faSearch, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 export function Navbar() {
@@ -29,12 +29,12 @@ export function Navbar() {
         <span><FontAwesomeIcon icon={faUtensils} className={style.faUtensils} />Resto.</span>
 
         <ul>
-          <Link to="/" className={style.active}>Home</Link>
-          <Link to="/dishes">Dishes</Link>
-          <Link to="/aboutUs">About</Link>
-          <Link to="/menu">Menu</Link>
-          <Link to="/review">Review</Link>
-          <Link to="/order">Order</Link>
+          <NavLink to="/" className={({ isActive }) => isActive ? style.active : ""}>Home</NavLink>
+          <NavLink className={({ isActive }) => isActive ? style.active : ""} to="/dishes">Dishes</NavLink>
+          <NavLink className={({ isActive }) => isActive ? style.active : ""} to="/aboutUs">About</NavLink>
+          <NavLink className={({ isActive }) => isActive ? style.active : ""} to="/menu">Menu</NavLink>
+          <NavLink className={({ isActive }) => isActive ? style.active : ""} to="/review">Review</NavLink>
+          <NavLink className={({ isActive }) => isActive ? style.active : ""} to="/order">Order</NavLink>
         </ul>
         <div className={style.NavIcon}>
           <FontAwesomeIcon icon={faSearch} className={style.FontAwesomeIcon} onClick={handleClick} />
