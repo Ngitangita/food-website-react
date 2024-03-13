f5bc939e54d53dd82297c810490cee2affed93c6
Ngitangita <hei.elise.3gmail.com>
2024-03-13 15:38:05 +0300

    feat: add rooter creatBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Dishes } from './componants/dishes/Dishes'
 import { Footer } from './componants/footer/Footer'
 import { Home } from './componants/home/Home'
@@ -9,21 +9,69 @@ import { Order } from './componants/order/Order'
 import { Review } from './componants/review/Review'
 import { About } from './componants/about/About'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'dishes',
+        element: <Dishes />
+      },
+      {
+        path: 'aboutUs',
+        element: <About />
+      },
+      {
+        path: 'menu',
+        element: <Menu />
+      },
+      {
+        path: 'review',
+        element: <Review />
+      },
+      {
+        path: 'order',
+        element: <Order />
+      }
+    ]
+  },
+  {
+    path: '/',
+    element: <Footer />
+  }
+])
+
 export default function App() {
+  return (<RouterProvider router={router}/>
+    // <>
+    //   <BrowserRouter >
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/dishes" element={<Dishes />} />
+    //       <Route path="/aboutUs" element={<About />} />
+    //       <Route path="/menu" element={<Menu />} />
+    //       <Route path="/review" element={<Review />} />
+    //       <Route path="/order" element={<Order />} />
+    //       <Route path="/footer" element={<Footer />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </>
+  )
+}
+
+
+
+function Root(){
   return (
     <>
-      <BrowserRouter >
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dishes" element={<Dishes />} />
-          <Route path="/aboutUs" element={<About />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/review" element={<Review />} />
-          <Route path="/order" element={<Order />} />
-          <Route path="/footer" element={<Footer />} />
-        </Routes>
-      </BrowserRouter>
+    <Navbar />
+    <Outlet />
     </>
   )
 }
